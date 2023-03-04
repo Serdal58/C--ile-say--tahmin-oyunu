3b87adf273416f96697a5612286eb5b177a2036c
Serdal58 <sserdalyildirim58@gmail.com>
2023-03-04 23:07:03 +0300

    Sayı tahmin oyunu
    Hafıza gelişimi için keyifli bir oyun

---
+++ Sayı tahmın oyunu/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projem2
+{
+    public partial class Form1 : Form
+    {
+        Image[] resımler =
+        {
+            Properties.Resources.at,
+            Properties.Resources.ejderha,
+             Properties.Resources.fıl,
+              Properties.Resources.kedı,
+                Properties.Resources.koala,
+                 Properties.Resources.penguen,
+                 Properties.Resources.zurafa,
+                 Properties.Resources.yunus,
+
+
+        };
+        int[] ındeksler = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resımlerıkarıstır();
+            Random rnd = new Random();
+
+            for (int i = 0; i < 16; i++)
+            {
+                int sayı = rnd.Next(16);
+                int gecıcı = ındeksler[i];
+                ındeksler[i] = ındeksler[sayı];
+                ındeksler[sayı] = gecıcı;
+
+
+            }
+        }
+
+        private void resımlerıkarıstır()
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox kutu = (PictureBox)sender;
+            int kutuNo = int.Parse(kutu.Name.Substring(10)) - 1;
+            kutu.Image = resımler[ındeksler[kutuNo]];
+
+        }
+    }
+}
---
+++ Sayı tahmın oyunu/Form2.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projem2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        int zaman = 6;
+        int puan = 0;
+        Random s = new Random();
+       
+
+        private void BTNBASLA_Click(object sender, EventArgs e)
+        {
+            int sayı1, sayı2, sayı3, sayı4, sayı5, sayı6, sayı7, sayı8, sayı9;
+            sayı1 = s.Next(0, 40);
+            sayı2 = s.Next(0, 40);
+            sayı3 = s.Next(0, 40);
+            sayı4 = s.Next(0, 40);
+            sayı5 = s.Next(0, 40);
+            sayı6 = s.Next(0, 40);
+            sayı7 = s.Next(0, 40);
+            sayı8=s.Next(0, 40);
+            sayı9 = s.Next(0, 40);
+
+            label1.Text = sayı1.ToString();
+            label2.Text = sayı2.ToString();
+            label2.Text = sayı2.ToString();
+            label3.Text = sayı3.ToString();
+            label4.Text = sayı4.ToString();
+            label5.Text = sayı5.ToString();
+            label6.Text = sayı6.ToString();
+            label7.Text = sayı7.ToString();
+            label8.Text = sayı8.ToString();
+            label9.Text = sayı9.ToString();
+
+            timer1.Start();
+            BTNBASLA.Enabled = false;
+
+            
+
+
+        }
+
+        private void btnkontol_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text==label1.Text&&textBox2.Text==label2.Text&&textBox3.Text==label3.Text&&textBox4.Text==label4.Text&&textBox5.Text==label5.Text&&textBox6.Text==label6.Text&&textBox7.Text==label7.Text&&textBox8.Text==label8.Text&&textBox9.Text==label9.Text)
+            {
+                MessageBox.Show("Tebrikler kazandınız");
+                puan += 10;
+                label12.Text = puan.ToString();
+                
+
+                
+                
+            }
+            else
+            {
+                MessageBox.Show("Kaybettiniz");
+            }
+           
+        
+        
+        
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            zaman--;
+            label10.Text = zaman.ToString();
+            if (zaman==0)
+            {
+                timer1.Stop();
+                groupBox1.Visible = false;
+                zaman = 6;
+
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            groupBox1.Visible = true;
+            timer1.Start();
+            button1.Enabled = false;
+
+
+
+        }
+
+        private void btnyenıden_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+
+
+        }
+    }
+}
